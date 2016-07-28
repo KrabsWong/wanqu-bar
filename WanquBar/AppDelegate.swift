3a3c9094d4e105a37f99cb3ae11c48e76750e616
Mr.Krabs <pangxie.wdy@gmail.com>
2016-07-28 18:47:40 +0800

    feat: 基础功能.. 能弹出来个popover出来. 代码基本上是照着葫芦画葫芦, 从网上找的... 出处太多..

---
+++ WanquBar/AppDelegate.swift
@@ -0,0 +1,54 @@
+//
+//  AppDelegate.swift
+//  WanquBar
+//
+//  Created by Pang.Xie on 16/7/28.
+//  Copyright © 2016年 Pang.Xie. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var wanquMenu: NSMenu!
+    @IBOutlet weak var wanquPopover: NSPopover!
+    
+    let wanquItem = NSStatusBar.systemStatusBar().statusItemWithLength(NSVariableStatusItemLength)
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+        if let button = wanquItem.button {
+            let icon = NSImage(named: "wanquIcon")
+            icon?.template = true;
+            button.image = icon
+            button.action = #selector(AppDelegate.togglePopover(_:))
+        }
+        
+        wanquPopover.contentViewController = WanquViewController(nibName: "WanquViewController", bundle: nil)
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+    
+    func showPopover(sender: AnyObject?) {
+        if let button = wanquItem.button {
+            wanquPopover.showRelativeToRect(button.bounds, ofView: button, preferredEdge: NSRectEdge.MinY)
+        }
+    }
+    
+    func closePopover(sender: AnyObject?) {
+        wanquPopover.performClose(sender)
+    }
+    
+    func togglePopover(sender: AnyObject?) {
+        if wanquPopover.shown {
+            closePopover(sender)
+        } else {
+            showPopover(sender)
+        }
+    }
+
+}
+
---
+++ WanquBar/WanquViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WanquViewController.swift
+//  WanquBar
+//
+//  Created by Pang.Xie on 16/7/28.
+//  Copyright © 2016年 Pang.Xie. All rights reserved.
+//
+
+import Cocoa
+
+class WanquViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
